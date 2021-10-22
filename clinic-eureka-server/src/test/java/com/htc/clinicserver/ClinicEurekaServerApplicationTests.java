cf4d4f0b80315c2c103125ceae027efe578fd61b
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-10-22 16:51:22 +0530

    Intial commit

---
+++ clinic-eureka-server/src/test/java/com/htc/clinicserver/ClinicEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htc.clinicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
